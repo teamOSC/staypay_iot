000ce01d9d3e3e18a7c2696a199e6b9de2865358
sheikebab <abhinav.b.sinha@gmail.com>
2015-07-19 03:22:56 +0530

    stop file to reset all gpio pins to 0
    Signed-off-by: sheikebab <abhinav.b.sinha@gmail.com>

---
+++ stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+
+echo 0 > /sys/class/gpio/gpio49/value
+echo 0 > /sys/class/gpio/gpio115/value
+echo 0 > /sys/class/gpio/gpio44/value
+echo 0 > /sys/class/gpio/gpio26/value
+echo 0 > /sys/class/gpio/gpio46/value
+echo 0 > /sys/class/gpio/gpio65/value
+
+
+
+
+
+
+
